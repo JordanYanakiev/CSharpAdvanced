7ecb4d02b6c1de556054143076b3fa6f7b84ca79
JordanYanakiev <jordan.yanakiev@gmail.com>
2022-09-29 22:30:30 +0300

    files and streams

---
+++ Files and Streams/01. Even Lines_Skeleton/Skeleton-Exercise/Skeleton/EvenLines/EvenLines.cs
@@ -0,0 +1,62 @@
+﻿
+
+
+namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            string readLain = string.Empty;
+            StreamReader sr = new StreamReader(inputFilePath);
+            int count = 0;
+
+            while (readLain != null)
+            {
+                readLain = sr.ReadLine();
+                string reverse = Reversewords(readLain);
+                string replacedSymbols = ReplaceSymbols(reverse);
+                
+
+                if (count % 2 == 0)
+                {
+                    Console.WriteLine(replacedSymbols);
+                }
+
+                count++;
+            }
+
+            return readLain;
+        }
+
+        private static string ReplaceSymbols(string reverse)
+        {
+            string[] symbolsToSwitch = { "-", ",", ".", "!", "?"};
+
+            foreach (var symbol in symbolsToSwitch)
+            {
+                reverse =  reverse.Replace(symbol, "@");
+            }
+
+            return reverse;
+        }
+
+        public static string Reversewords(string readedLine)
+        {
+            string[] strToReverse = readedLine.Split(" ").Reverse().ToArray();
+            //Console.WriteLine(strToReverse);
+            return string.Join(" ", strToReverse);
+        }
+    }
+}
---
+++ Files and Streams/01. Even Lines_Skeleton/Skeleton-Exercise/Skeleton/LineNumbers/LineNumbers.cs
@@ -0,0 +1,81 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string readLine = string.Empty;
+            StreamReader sr = new StreamReader(inputFilePath);
+            //FileStream sWrt = new FileStream(outputFilePath, FileMode.Create());
+            StringBuilder sb = new StringBuilder();
+            int counter = 1; 
+
+            while (readLine != null)
+            {
+                readLine = sr.ReadLine();
+                int countSymbols = CountSymbols(readLine);
+                int countLetters = CountLetters(readLine);
+
+                if (readLine != null)
+                {
+                    string lineNumber = $"Line {counter}: " + readLine + $" ({countLetters})({countSymbols}) ";
+                    sb.AppendLine(lineNumber);
+                    //Console.WriteLine(lineNumber);
+                }
+                counter++;
+            }
+
+            File.WriteAllText(outputFilePath, sb.ToString());
+
+        }
+
+        private static int CountSymbols(string readLine)
+        {
+            int symbolsCount = 0;
+
+            if (readLine != null)
+            {
+                for (int i = 0; i < readLine.Length; i++)
+                {
+                    if (readLine[i] == '?' || readLine[i] == '!' || readLine[i] == '-' || readLine[i] == '.'
+                        || readLine[i] == ',' || readLine[i] == '\'')
+                    {
+                        symbolsCount++;
+                    }
+                }
+            }
+
+            return symbolsCount;
+        }
+        
+        private static int CountLetters(string readLine)
+        {
+            int lettersCount = 0;
+
+            if (readLine != null)
+            {
+                for (int i = 0; i < readLine.Length; i++)
+                {
+                    if (char.IsLetter(readLine[i]))
+                    {
+                        lettersCount++;
+                    }
+                }
+            }
+
+            return lettersCount;
+        }
+    }
+}
